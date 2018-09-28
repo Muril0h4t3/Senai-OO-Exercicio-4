1402e0aeb9c5ee2e7b9ab6976ea62aa55c1f82dd
Murilo Dos Santos Gregorio <50981737870@info.local>
2018-09-28 10:40:38 -0300

    msg

---
+++ Classes/Cartao.cs
@@ -0,0 +1,13 @@
+namespace senai.OO.exercicio4.Classes
+{
+    public class Cartao
+    {
+        public string ndocartao;
+
+        public int vencimentod;
+
+        public int vencimentoM;
+
+        public int vencimentoy;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using senai.OO.exercicio4.Classes;
+
+namespace senai.OO.exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 
+            Cartao cartao1 = new Cartao();
+
+            Console.WriteLine("Insira o nimero do cartâo");
+            cartao1.ndocartao = Console.ReadLine();
+
+            Console.WriteLine("Insira o dia de vencimento do cartão");
+            cartao1.vencimentod = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o mês de vencimento do cartão");
+            cartao1.vencimentoM = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o ano de vencimento do cartão");
+            cartao1.vencimentoy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O cartão de número {cartao1.ndocartao} Vence {cartao1.vencimentod}/{cartao1.vencimentoM}/{cartao1.vencimentoy}");
+            #endregion 
+
+            #region 
+            Cartao cartao2 = new Cartao();
+
+            var dateTime = DateTime.Parse("10/05/1995");
+           
+
+            Console.WriteLine("Insira o nimero do cartâo");
+            cartao2.ndocartao = Console.ReadLine();
+
+            Console.WriteLine("Insira o dia de vencimento do cartão");
+            cartao2.vencimentod = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o mês de vencimento do cartão");
+            cartao2.vencimentoM = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o ano de vencimento do cartão");
+            cartao2.vencimentoy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O cartão de número {cartao2.ndocartao} Vence {cartao2.vencimentod}/{cartao2.vencimentoM}/{cartao2.vencimentoy}");
+            #endregion 
+        }
+    }
+}
